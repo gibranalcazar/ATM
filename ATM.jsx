634282e275c80e6ceacfe6570c57d4ca58af053c
GN-AM <gibran@live.com>
2022-07-09 20:56:09 -0500

    S1

--- ATM.jsx
+++ ATM.jsx
@@ -1,18 +1,29 @@
-const ATMDeposit = ({handleChange, isDeposit}) =>{
+const ATMDeposit = ({handleChange, eventSelected, isDeposit}) =>{
   const choice = ["Deposit", "Cash Back"];
-  return(
-    <label className="label huge">
-      <h3>{choice[Number(!isDeposit)]}</h3>
-      <input id="inputValue" type="number" placeholder="Amount" onChange={handleChange}></input>
-      <button className="btn btn-primary" type="submit" value="submit">Submit</button>
-      
-    </label>
-  )
+  console.log(eventSelected);
+  if (eventSelected) {
+    return(
+      <label className="label huge">
+        <h3>{choice[Number(!isDeposit)]}</h3>
+        <input id="inputValue" type="number" placeholder="Amount" onChange={handleChange}></input>
+        <button className="btn btn-primary" type="submit" value="submit">Submit</button>
+        
+      </label>
+    )
+  } else{
+    return(
+      <label className="label huge">
+        <h3>Select operation</h3>
+      </label>
+    )
+  }
+ 
 }
 
 const Account = () => {
   const [totalState, setTotalState] = React.useState(4000);
   const [isDeposit, setIsDeposit] = React.useState(true);
+  const [eventSelected, setEventSelected] = React.useState(false);
   let transactionState = 0; // state of this transaction
   let status = `Account Balance $${totalState}`;
 
@@ -42,10 +53,10 @@ const Account = () => {
     <form className="container app" onSubmit={handleSubmit}>
       <h2 id="total">{status}</h2>
       <div className="buttons">
-        <button className="button" onClick={()=> setIsDeposit(true)}>Deposit</button>
-        <button className="button" onClick={()=> setIsDeposit(false)}>Cash Back</button>
+        <button className="button" onClick={()=> {setIsDeposit(true); setEventSelected(true)}}>Deposit</button>
+        <button className="button" onClick={()=> {setIsDeposit(false); setEventSelected(true)}}>Cash Back</button>
       </div>
-      <div><ATMDeposit handleChange={handleChange} handleSubmit={handleSubmit} isDeposit={isDeposit} /></div>
+      <div><ATMDeposit handleChange={handleChange} eventSelected={eventSelected} isDeposit={isDeposit} /></div>
     </form>
   ) 
 }
